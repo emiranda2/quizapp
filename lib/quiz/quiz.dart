de8c116c3394b5be11238d6578b2bec6a47ffa9e
Ethan Miranda <emirand2@uwo.ca>
2025-02-07 16:05:07 -0500

    feat: bottom sheet

--- lib/quiz/quiz.dart
+++ lib/quiz/quiz.dart
@@ -126,11 +126,109 @@ class CongratsPage extends StatelessWidget {
 }
 
 class QuestionPage extends StatelessWidget {
-  const QuestionPage({super.key, required this.question});
   final Question question;
+  const QuestionPage({super.key, required this.question});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    var state = Provider.of<QuizState>(context);
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        Expanded(
+          child: Container(
+            padding: const EdgeInsets.all(16),
+            alignment: Alignment.center,
+            child: Text(question.text),
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: question.options.map(
+              (opt) {
+                return Container(
+                  height: 90,
+                  margin: const EdgeInsets.only(bottom: 10),
+                  color: Colors.black26,
+                  child: InkWell(
+                    onTap: () {
+                      state.selected = opt;
+                      _bottomSheet(context, opt, state);
+                    },
+                    child: Container(
+                      padding: const EdgeInsets.all(16),
+                      child: Row(
+                        children: [
+                          Icon(
+                              state.selected == opt
+                                  ? FontAwesomeIcons.circleCheck
+                                  : FontAwesomeIcons.circle,
+                              size: 30),
+                          Expanded(
+                            child: Container(
+                              margin: const EdgeInsets.only(left: 16),
+                              child: Text(
+                                opt.value,
+                                style: Theme.of(context).textTheme.bodyMedium,
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ).toList(),
+          )
+        )
+      ]
+    );
+  }
+
+    /// Bottom sheet shown when Question is answered
+  _bottomSheet(BuildContext context, Option opt, QuizState state) {
+    bool correct = opt.correct;
+
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+          height: 250,
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text(correct ? 'Good Job!' : 'Wrong'),
+              Text(
+                opt.detail,
+                style: const TextStyle(fontSize: 18, color: Colors.white54),
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: correct ? Colors.green : Colors.red),
+                child: Text(
+                  correct ? 'Onward!' : 'Try Again',
+                  style: const TextStyle(
+                    color: Colors.white,
+                    letterSpacing: 1.5,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                onPressed: () {
+                  if (correct) {
+                    state.nextPage();
+                  }
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
+        );
+      },
+    );
   }
 }
