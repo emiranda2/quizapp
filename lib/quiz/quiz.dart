6f2179e65e3ef57e9619f5530990d06215f80079
Ethan Miranda <emirand2@uwo.ca>
2025-02-07 15:47:09 -0500

    feat: dynamic screens

--- lib/quiz/quiz.dart
+++ lib/quiz/quiz.dart
@@ -29,12 +29,108 @@ class QuizScreen extends StatelessWidget {
                 leading: IconButton(
                   icon: const Icon(FontAwesomeIcons.xmark),
                   onPressed: () => Navigator.pop(context),
-                )
-              )
+                ),
+              ),
+              body: PageView.builder(
+                physics: const NeverScrollableScrollPhysics(),
+                scrollDirection: Axis.vertical,
+                controller: state.controller,
+                onPageChanged: (int idx) =>
+                  state.progress = (idx / (quiz.questions.length + 1)),
+                  itemBuilder: (BuildContext context, int idx) {
+                    if(idx == 0) {
+                      return StartPage(quiz: quiz);
+                    } else if(idx == quiz.questions.length + 1) {
+                      return CongratsPage(quiz: quiz);
+                    } else {
+                      return QuestionPage(question: quiz.questions[idx - 1]);
+                    }
+                  },
+              ),
             );
           }
         }
       )
     );
+  }
+}
+
+class StartPage extends StatelessWidget {
+  const StartPage({super.key, required this.quiz});
+  final Quiz quiz;
+  @override
+  Widget build(BuildContext context) {
+    var state = Provider.of<QuizState>(context);
+
+    return Container(
+      padding: const EdgeInsets.all(20),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(quiz.title, style: Theme.of(context).textTheme.headlineMedium),
+          const Divider(),
+          Expanded(child: Text(quiz.description)),
+          OverflowBar(
+            alignment: MainAxisAlignment.center,
+            children: <Widget>[
+              ElevatedButton.icon(
+                onPressed: () => state.nextPage(),
+                label: const Text('Start Quiz!'),
+                icon: const Icon(Icons.poll)
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class CongratsPage extends StatelessWidget {
+  final Quiz quiz;
+  const CongratsPage({super.key, required this.quiz});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Congrats! You completed the ${quiz.title} quiz',
+            textAlign: TextAlign.center,
+          ),
+          const Divider(),
+          Image.asset('assets/congrats.gif'),
+          const Divider(),
+          ElevatedButton.icon(
+            style: TextButton.styleFrom(
+              backgroundColor: Colors.green,
+            ),
+            icon: const Icon(FontAwesomeIcons.check),
+            label: const Text(' Mark Complete!'),
+            onPressed: () {
+              SupabaseService().updateUserReport(quiz);
+              Navigator.pushNamedAndRemoveUntil(
+                context,
+                '/topics',
+                (route) => false,
+              );
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class QuestionPage extends StatelessWidget {
+  const QuestionPage({super.key, required this.question});
+  final Question question;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
